85a0a12234d9f7e890d75b584b0c13896dd6afc9
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2023-10-15 14:40:01 -0300

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,8 @@
 import biblioteca from '../../img/biblioteca.png'
 import arrow from '../../img/arrow.svg';
 import TabComponent from '../../components/Tabs/Tabs';
-
+import { Link } from 'react-router-dom'
+import './Home.css'
 
 export default function Home(){
     return (
@@ -10,13 +11,16 @@ export default function Home(){
           <div className='imgHome'> <img className='bgImg' src={ biblioteca } alt='imagem biblioteca'/> 
           <div className='infoBox'>
             <h1>¶ Vestibuloteca </h1>
-    
-            <a href='#Tabs'>Veja todos os livros <img className='btnArrow' src= {arrow} alt='seta'/> </a>
+            <Link to='/tabs'>
+              <p>Veja todos os livros <img className='btnArrow' src= {arrow} alt='seta'/> </p>
+            </Link>
+            
           </div>
           </div>
     
           <section className='slogan'>
             <h1>¶ Vestibuloteca</h1>
+            
             <p>Todas as obras literárias dos maiores vestibulares brasileiros em um só lugar!</p>
           </section>
 
